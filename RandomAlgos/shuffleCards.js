2bd9a93522555bc53a07e3be785250d674cc2e5d
Leslie <lesliew1993@gmail.com>
2020-04-16 14:35:51 -0400

    added the solutions to shuffleCards solutions

--- RandomAlgos/shuffleCards.js
+++ RandomAlgos/shuffleCards.js
@@ -1,7 +1,47 @@
-let deck = [2, 3, 4, 5, 6, 7, 8, 9, 19, 'J', 'Q', 'K', 'A'];
+let deck = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A'];
+
+//Solution 1
+const swap = (cards, idx) => {
+	let randIdx = Math.floor(Math.random() * (cards.length - 1));
+	let placeholder = cards[randIdx];
+	cards[randIdx] = cards[idx];
+	cards[idx] = placeholder;
+};
 
 const shuffle = (cards) => {
-  //map through cards
-}
+	cards.forEach((card, idx) => {
+		swap(cards, idx);
+	});
+	return cards;
+};
 
-shuffle(deck);
+shuffle(deck) //==> should return a shuffled deck
+
+//Solution 2
+
+// const swap = (cards, idx) => {
+// 	let randIdx = Math.floor(Math.random() * (cards.length - 1));
+// 	[cards[randIdx], cards[idk]] = [cards[idx], cards[randIdx]];
+// };
+
+// const shuffle = (cards) => {
+// 	cards.forEach((card, idx) => {
+// 		swap(cards, idx);
+// 	});
+// 	return cards;
+// };
+
+//Solution 3 - with cloned array
+
+// const swap = (cards, idx) => {
+// 	let randIdx = Math.floor(Math.random() * (cards.length - 1));
+// 	[cards[randIdx], cards[idk]] = [cards[idx], cards[randIdx]];
+// };
+
+// const shuffle = (cards) => {
+// 	let clone = [...cards];
+// 	clone.forEach((card, idx) => {
+// 		swap(clone, idx);
+// 	});
+// 	return clone;
+// };
