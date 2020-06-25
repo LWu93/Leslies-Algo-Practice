b249600836012c7b1fa9defa7641428fe8d07f2d
Leslie <lesliew1993@gmail.com>
2020-06-24 22:18:29 -0400

    Add LC June Challenge #24

---
+++ Leetcode/June2020MonthyChallenge/#24-uniqueBSTs.js
@@ -0,0 +1,10 @@
+var numTrees = function(n) {
+    let result = [1];
+    for (let i = 1; i <= n; i++) {
+        result[i] = 0;
+        for (let j = 0; j < i; j++) {
+            result[i] += (result[j] * result[i-j-1]);
+        }
+    }
+    return result[n];
+};
