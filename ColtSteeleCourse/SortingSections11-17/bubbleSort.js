5ff325361a670b53c9edcab57bfdcbd5de0eb730
Leslie <lesliew1993@gmail.com>
2020-06-29 17:39:54 -0400

    Add - bubblesort Optimizations and added time/space complexity

--- ColtSteeleCourse/SortingSections11-17/bubbleSort.js
+++ ColtSteeleCourse/SortingSections11-17/bubbleSort.js
@@ -10,13 +10,21 @@ const bubbleSort = (arr) => {
   //if the first element is > than the next, swap them
   //keep going until you've moved the greatest element to the back every i loop
   for (let i = arr.length; i > 0; i--) {
+    //check to see if we ever make any swaps in our first for loop
+    let noSwaps = true
     for (let j = 0; j < i - 1; j++) {
       if (arr[j] > arr[j+1]) {
         swap(arr, j, j+1)
+        noSwaps = false
       }
     }
+    //if we make no swaps, we are done and they're sorted. simply break
+    if (noSwaps) break;
   }
   return arr
 }
 
 bubbleSort([5, 2, 6, 19, 23, 15, 24]) //[2,5,6,15,19,23,24]
+
+//Time Complexity - O(n^2) worse case is we full go through easy nested for loop
+//Space Complexity - O(1) swapping inplace
