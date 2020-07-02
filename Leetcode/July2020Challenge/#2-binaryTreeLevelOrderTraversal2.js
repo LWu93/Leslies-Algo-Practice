df41a5900119c174a5f26cc74dce426b0e226837
Leslie <lesliew1993@gmail.com>
2020-07-02 11:02:41 -0400

    Add - additional notes and diagram for July #2 QOTD

--- Leetcode/July2020Challenge/#2-binaryTreeLevelOrderTraversal2.js
+++ Leetcode/July2020Challenge/#2-binaryTreeLevelOrderTraversal2.js
@@ -1,8 +1,35 @@
+/*
+
+Given a binary tree, return the bottom-up level order traversal of its nodes' values. (ie, from left to right, level by level from leaf to root).
+
+For example:
+Given binary tree [3,9,20,null,null,15,7],
+    3
+   / \
+  9  20
+    /  \
+   15   7
+
+return its bottom-up level order traversal as:
+
+[
+  [15,7],
+  [9,20],
+  [3]
+]
+
+*/
+
+//Psudocode
+//Traverse tree BFS and implement a queue
+//loop through its children and push vals into arr.
+//If those nodes have children, push into queue.
+//push nodeVals array into the res arr. Return res reversed.
 
 //Iterative
 var levelOrderBottom = function(root) {
-    let arr = []
-    if (!root) return arr;
+    let res = []
+    if (!root) return res;
     let q = [root]
 
     while (q.length) {
@@ -16,10 +43,10 @@ var levelOrderBottom = function(root) {
             if (curr.right) q.push(curr.right)
         }
 
-        arr.push(nodeVals)
+        res.push(nodeVals)
     }
 
-    return arr.reverse()
+    return res.reverse()
 };
 
 //Time Complexity - O(N). N - number of nodes in tree and we're looping through each node in the queue.
